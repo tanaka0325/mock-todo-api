a2b25a9de80443f1f899f7393f1d90bb412430b6
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-02-07 22:40:25 +0900

    Add "Read" routes for CRUD

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+require './models/todos.rb'
+
 class MockTodoApp < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
@@ -12,6 +14,20 @@ class MockTodoApp < Sinatra::Base
 
   get '/' do
     "hello world!"
+  end
+
+  get '/todos/?' do
+    todos = Todo.all
+    return todos.to_json
+  end
+
+  get '/todos/:id' do
+    begin
+      todo = Todo.find(params[:id])
+    rescue => e
+      return "Couldn't find Todo with 'id'=#{params[:id]}"
+    end
+    return todo.to_json
   end
 
 end
